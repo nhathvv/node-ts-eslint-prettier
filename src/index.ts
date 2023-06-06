74fcd1870dcddd13cdc89403743e37a2cd64e838
Nhathv21 <Nhathv.21it@gmail.com>
2023-06-06 23:11:18 +0700

    debug run & vscode

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-type Handle = () => Promise<string>
-const fullName: Handle = () => Promise.resolve('Nhathv')
-fullName().then((res) => {
-  console.log(res)
+import express from 'express'
+import { sum } from './utils'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  const data: any = null
+  const result = sum(data)
+  console.log(result)
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
---
+++ src/utils.ts
@@ -0,0 +1,2 @@
+const sum = (obj: { a: number; b: number }) => obj.a + obj.b
+export { sum }
